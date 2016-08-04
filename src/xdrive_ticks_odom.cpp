60628b877fcd1887611497e9daa0e3589c06e20d
qqtk <qilong.cn@163.com>
2016-08-04 11:54:47 +0800

    Update xdrive_ticks_odom.cpp

--- src/xdrive_ticks_odom.cpp
+++ src/xdrive_ticks_odom.cpp
@@ -14,6 +14,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Quaternion.h>
 
 #include "robbase_msg/encoders.h"
