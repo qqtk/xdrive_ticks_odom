82302f75f277ad1e11e15dd3bf3cf33b65b328a7
qqtk <qilong.cn@163.com>
2016-08-05 10:24:11 +0800

    Update xdriver.h

--- include/xdrive_ticks_odom/xdriver.h
+++ include/xdrive_ticks_odom/xdriver.h
@@ -1,22 +1,14 @@
 #ifndef _H_XDRIVER
 #define _H_XDRIVER
-
+// file : xdriver.h
 #ifdef __cplusplus
 extern "C" {
 #endif  /* __cplusplus */
 
-	#define TAG_NAME_VEL_LINE "VEL_LINE"
-	#define TAG_NAME_VEL_THETA "VEL_THETA"
 
 	#define MAKEIPADDRESS(b1,b2,b3,b4) ( ((unsigned int)b1<<24)+( (unsigned int)b2<<16)+ ((unsigned int)b3<<8)+b4 )
 
 	#define CONTROLLER_IP MAKEIPADDRESS(192,168,1,33)
-
-	float xdriver_getVel_line();
-	bool xdriver_setVel_line(float vel);
-
-	float xdriver_getVel_theta();
-	bool xdriver_setVel_theta(float vel);
 
 	unsigned int xdriver_getValue(const char *tagName);
 	bool xdriver_setValue(const char *tagName, const unsigned int value);
@@ -24,6 +16,11 @@ extern "C" {
 	float xdriver_getValue_float(const char *tagName);
 	bool xdriver_setValue_float(const char *tagName, const float value);
 
+	// read/write in memory format, <int len> = num-of-total-bytes.
+	bool xdriver_get(const char *tagName,void *pData,unsigned int len);
+	bool xdriver_set(const char *tagName,const void *pData,unsigned int len);
+	
+	// to be deleted:
 	unsigned int xdriver_struct_get(const char *tagName,void *structData);
 	bool xdriver_struct_set(const char *tagName, void *structData);
 
